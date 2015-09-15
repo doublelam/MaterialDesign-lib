67f02f5183cb09500786e5416b92e2e7ce883229
lam <1422015387@qq.com>
2015-09-15 16:38:05 +0800

    Initial commit

---
+++ src/scripts/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Hoofei.
+ */
+
+$(function() {
+    'use strict';
+    window.FastClick.attach(document.body);
+    // Start here...
+});
+
+$(document.body).css("background:red");
